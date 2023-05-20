76a9c9895bf4f3b68d7870213a60c9d4180f9f9b
a1127228575@126.com <a1127228575@126.com>
2023-05-21 04:40:17 +0800

    把依赖的vendor改为github的mod

--- vendor/lzh/goframework/server/http/run.go
+++
@@ -1,9 +0,0 @@
-package http
-
-import "time"
-
-type ServerConfig struct {
-	Address      string        `yaml:"address"`
-	ReadTimeout  time.Duration `yaml:"readtimeout"`
-	WriteTimeout time.Duration `yaml:"writetimeout"`
-}
--- vendor/lzh/goframework/server/http/run_windows.go
+++
@@ -1,9 +0,0 @@
-package http
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func Start(engine *gin.Engine, conf ServerConfig) error {
-	return engine.Run(conf.Address)
-}
